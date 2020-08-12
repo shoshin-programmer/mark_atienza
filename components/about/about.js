b766e274c102086bb098399b6a8a53861de72d3d
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-08-12 20:15:15 +0800

    added about

--- components/about/about.js
+++ components/about/about.js
@@ -6,7 +6,14 @@ import styles from "./about.module.css";
 export default function About() {
   return (
     <Container>
-      <div className={styles.header}>about</div>
+      <div className={styles.aboutContainer}>
+        <div className={styles.header}>about.</div>
+        <div className={styles.contain}>
+          <div className={styles.text}>
+            I create minimalist websites that is focused for the users.
+          </div>
+        </div>
+      </div>
     </Container>
   );
 }
