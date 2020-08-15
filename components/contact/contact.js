7d7e26832f8156ce3ca38dc77306f397f6779456
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-08-15 19:06:22 +0800

    added contact header

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -7,11 +7,16 @@ export default function Contact() {
   return (
     <Container>
       <div className={styles.contactContainer} id="contact">
+        <div className={styles.header}>contact.</div>
         <div className={styles.contain}>
           <div className={styles.text}>
             <a href="mailto:atienza.new@gmail.com">
               <button className={styles.contact}>CONTACT ME</button>
             </a>
+            <p>
+              I'll be happy to talk about projects and network for future
+              endeavours.
+            </p>
           </div>
         </div>
       </div>
