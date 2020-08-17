e99a1287bfd72bc054e32649a538fcf10eb6d4e5
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-08-17 21:43:03 +0800

    added preloader

---
+++ components/loaders/full_loader.js
@@ -0,0 +1,12 @@
+// Dependencies
+// Components
+// Assets
+import styles from "./loaders.module.css";
+
+export default function FullLoader() {
+  return (
+    <div className={styles.fullPageWrap}>
+      <div className={styles.box}></div>
+    </div>
+  );
+}
