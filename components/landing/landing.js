0e8eb0453b1d270fb60dc66c8b419cb23d79f0ce
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-08-19 22:35:45 +0800

    centered text

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -8,23 +8,21 @@ import styles from "./landing.module.css";
 export default function Landing() {
   return (
     <Container>
-      <div className={styles.left}>
-        <Fade bottom cascade delay={4700}>
-          <span className={styles.title}>Mark</span>
-          <br />
-          <span className={styles.title}>Atienza</span>
-          <br />
-          <span className={styles.subtitle}>Fullstack web developer</span>
-        </Fade>
-      </div>
-      <div className={styles.rightContainer}>
-        <Fade bottom cascade delay={5000}>
-          <div className={styles.right}>
-            <p className={styles.semititle}>
-              I develop beautiful
-              <br />
-              websites.
-            </p>
+      <div className={styles.aboutContainer} id="about">
+        <Fade bottom cascade>
+          <div className={styles.header}>mark atienza.</div>
+          <div className={styles.contain}>
+            <div className={styles.content}>
+              <p className={styles.subtitle}>[ fullstack developer ]</p>
+              <p className={styles.text}>
+                I am a fullstack developer based on Taguig City Philippines. I
+                create beautiful websites, efficient internal systems and other
+                things web.
+              </p>
+              <a href="mailto:atienza.new@gmail.com">
+                <button className={styles.contact}>CONTACT ME</button>
+              </a>
+            </div>
           </div>
         </Fade>
       </div>
