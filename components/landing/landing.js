6dc263d3826946ba03a6a0ddecdfeaaae045f8f7
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-08-19 22:41:24 +0800

    timed fade effect

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -9,16 +9,18 @@ export default function Landing() {
   return (
     <Container>
       <div className={styles.aboutContainer} id="about">
-        <Fade bottom cascade>
+        <Fade bottom cascade delay={5000}>
           <div className={styles.header}>mark atienza.</div>
           <div className={styles.contain}>
             <div className={styles.content}>
-              <p className={styles.subtitle}>[ fullstack developer ]</p>
-              <p className={styles.text}>
-                I am a fullstack developer based on Taguig City Philippines. I
-                create beautiful websites, efficient internal systems and other
-                things web.
-              </p>
+              <Fade bottom cascade delay={5300}>
+                <p className={styles.subtitle}>[ fullstack developer ]</p>
+                <p className={styles.text}>
+                  I am a fullstack developer based on Taguig City Philippines. I
+                  create beautiful websites, efficient internal systems and
+                  other things web.
+                </p>
+              </Fade>
               <a href="mailto:atienza.new@gmail.com">
                 <button className={styles.contact}>CONTACT ME</button>
               </a>
