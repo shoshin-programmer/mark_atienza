c758a9a32f03407c9fcef54b336a32765b817ee2
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-08-26 18:33:09 +0800

    updated work

--- components/work/work.js
+++ components/work/work.js
@@ -1,4 +1,5 @@
 // Dependencies
+import React from "react";
 import Fade from "react-reveal/Fade";
 // Components
 import Container from "../container.js";
@@ -6,24 +7,93 @@ import Container from "../container.js";
 import styles from "./work.module.css";
 
 export default function Work() {
+  const [datas, setDatas] = React.useState([
+    {
+      name: "ZEN Rooms",
+      position: "[ Python Developer ]",
+      description:
+        "Working on internal system for Finance team automation of processes."
+    },
+    {
+      name: "RingCentral",
+      position: "[ GSS PMO Analyst / Python Developer ]",
+      description:
+        "Internal HR system using Python, Django, React. From designing to deploying internal tool. Also worked with Microsoft PowerApps and Sharepoint for specific purposes."
+    },
+    {
+      name: "SiP",
+      position: "[ Data Specialist ]",
+      description:
+        "Data-mining, data-analysis, Facebook Marketing and automating Internal Processes. Stack used is mostly Python with Pandas, Matplotlib, Numpy, and Tkinter."
+    }
+  ]);
+  const [index, setIndex] = React.useState(0);
+  const [company, setCompany] = React.useState(datas[index]);
+  const [changing, setChanging] = React.useState(false);
+
+  const handleChange = change => {
+    setChanging(true);
+
+    const timer = setTimeout(() => {
+      const new_index = index;
+      if (change === "add") {
+        setIndex(new_index + 1);
+        setCompany(datas[index + 1]);
+      } else {
+        setIndex(new_index - 1);
+        setCompany(datas[index - 1]);
+      }
+      setChanging(false);
+    }, 300);
+  };
+
   return (
     <Container>
       <div className={styles.workContainer} id="work">
-        <Fade bottom cascade>
-          <div className={styles.header}>work.</div>
-          <div className={styles.contain}>
-            <div className={styles.mainText}>
-              RingCentral
-              <br />
-              <div className={styles.text}>[ Python Developer ]</div>
-              <br />
-              <div className={styles.description}>
-                Internal HR system using Python, Django, React. Designing and
-                deploying for users.
-              </div>
+        <div className={styles.header}>work.</div>
+        <div className={styles.contain}>
+          <div
+            className={`${styles.mainText} ${
+              changing ? styles.fadeOut : styles.fadeIn
+            }`}
+          >
+            {company.name}
+            <br />
+            <div
+              className={`${styles.text} ${
+                changing ? styles.fadeOut : styles.fadeIn
+              }`}
+            >
+              {company.position}
             </div>
+            <br />
           </div>
-        </Fade>
+
+          {index !== 0 ? (
+            <i
+              class={`${styles.arrowLeft} ${styles.arrow}`}
+              onClick={() => handleChange()}
+            ></i>
+          ) : (
+            ""
+          )}
+          <div
+            className={`${styles.description} ${
+              changing ? styles.fadeOut : styles.fadeIn
+            }`}
+          >
+            {company.description}
+          </div>
+
+          {index !== datas.length - 1 ? (
+            <i
+              class={`${styles.arrowRight} ${styles.arrow}`}
+              onClick={() => handleChange("add")}
+            ></i>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </Container>
   );
